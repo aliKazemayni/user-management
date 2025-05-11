d690b5ee72efb143e7a220be01d87399ab88e3ef
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-05-11 21:17:08 +0330

    feat(config): add error method in log

--- helpers/Log.php
+++ helpers/Log.php
@@ -4,11 +4,33 @@ namespace Helpers;
 
 class Log
 {
-    private static string $style = "background:#040404; color:#FF9F00; padding:10px; border:1px solid #ccc;";
+    private static string $style = "
+        background:#040404;
+        color:#FF9F00;
+        padding:10px;
+        font-size:0.75rem;
+        font-weight: bold;
+        border-radius: 10px;
+        border:1px solid #ccc;
+    ";
+
+    private static string $errorStyle = "       
+        color:#F3F3E0;
+        font-size:1rem;
+        font-weight: bold;       
+        padding:10px;
+        background: #BE5B50;
+        border-radius: 10px;        
+    ";
 
     public static function print(mixed $value, bool $isDie = false): void
     {
         self::pre($value,'print_r',$isDie);
+    }
+
+    public static function error(mixed $value, bool $isDie = false): void
+    {
+        self::pre($value,'print_r',$isDie, self::$errorStyle);
     }
 
     public static function dump(mixed $value, bool $isDie = false): void
@@ -16,11 +38,13 @@ class Log
         self::pre($value,'var_dump',$isDie);
     }
 
-    private static function pre(mixed $value, string $method, bool $isDie): void
+    private static function pre(mixed $value, string $method, bool $isDie, string $style = null): void
     {
-        echo "<pre style='".self::$style."'>";
+        $finalStyle = $style ?? self::$style;
+        echo "<pre style='$finalStyle'>";
         $method($value);
         echo "</pre>";
         $isDie && die();
     }
+
 }
