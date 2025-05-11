1e226f61acb1b9776529b81ac266ff3fa5f7a8c2
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-05-11 21:30:43 +0330

    feat(Log): add write to file

--- helpers/Log.php
+++ helpers/Log.php
@@ -4,6 +4,8 @@ namespace Helpers;
 
 class Log
 {
+    private static string $logPath = __DIR__ . '/../storage/logs/app.log';
+
     private static function getStyle(string $type = 'default'): string
     {
         return match ($type) {
@@ -27,19 +29,22 @@ class Log
         };
     }
 
-    public static function print(mixed $value, bool $isDie = false): void
+    public static function print(mixed $value, bool $isDie = false, $saveInFile = false): void
     {
         self::pre($value, 'print_r', $isDie);
+        $saveInFile && self::writeToFile($value, 'info');
     }
 
-    public static function error(mixed $value, bool $isDie = false): void
+    public static function error(mixed $value, bool $isDie = false, $saveInFile = false): void
     {
         self::pre($value, 'print_r', $isDie, 'error');
+        $saveInFile && self::writeToFile($value, 'error');
     }
 
-    public static function dump(mixed $value, bool $isDie = false): void
+    public static function dump(mixed $value, bool $isDie = false, $saveInFile = false): void
     {
         self::pre($value, 'var_dump', $isDie);
+        $saveInFile && self::writeToFile($value, 'info');
     }
 
     private static function pre(mixed $value, string $method, bool $isDie, string $style = 'default'): void
@@ -49,6 +54,21 @@ class Log
         $method($value);
         echo "</pre>";
         $isDie && die();
+    }
+
+    private static function writeToFile(mixed $data, string $level): void
+    {
+        $timestamp = date('Y-m-d H:i:s');
+        $formatted = is_string($data) ? $data : var_export($data, true);
+        $logEntry = "[$timestamp] [$level] $formatted" . PHP_EOL;
+
+        $logDir = dirname(self::$logPath);
+
+        if (!is_dir($logDir)) {
+            mkdir($logDir, 0777, true);
+        }
+
+        file_put_contents(self::$logPath, $logEntry, FILE_APPEND);
     }
 
 }
