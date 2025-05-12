d45f71a447c5fcfaaf9944dceae18480360a2aa1
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-05-13 00:44:42 +0330

    feat(config): add env load and use for time zone and debug mode in index

--- public/index.php
+++ public/index.php
@@ -1,8 +1,22 @@
 <?php
 
 use Core\Router;
+use Dotenv\Dotenv;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../routes/web.php';
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+date_default_timezone_set($_ENV['TIME_ZONE']);
+
+if ($_ENV['APP_DEBUG'] === 'true') {
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+} else {
+    error_reporting(0);
+    ini_set('display_errors', '0');
+}
 
 Router::dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
