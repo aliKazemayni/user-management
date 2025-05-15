616ba59d841703249a092e7ca805fe9762150a48
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-05-15 20:07:54 +0330

    feat(Database): add bind and construct for get query

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -2,7 +2,28 @@
 
 namespace Core\Database;
 
+use PDOException;
+use Core\Database\Database;
+
 class QueryBuilder
 {
+    private PDOStatement $builder;
+    public function __construct(string $sql)
+    {
+        try {
+            $this->builder = Database::connect()->prepare($sql);
+        } catch (PDOException $e) {
+            die("Prepare failed: " . $e->getMessage());
+        }
+    }
 
+    public function bind(array $params = []): self
+    {
+        try {
+            $this->builder->execute($params);
+        } catch (PDOException $e) {
+            die("Execute failed: " . $e->getMessage());
+        }
+        return $this;
+    }
 }
