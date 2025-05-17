54b0f933a48650f25019cccf1bfb7684c970e8f3
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-05-17 12:06:06 +0330

    fix(namespace): fix route class namespace in web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Controllers\HomeController;
-use core\Router;
+use Core\Router;
 
 Router::get('/', [HomeController::class, 'index']);
 Router::get('about', [HomeController::class, 'about']);
