54a69ff0cc9d46e066bd72d3a1478b93d672a3f1
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-05-19 15:53:10 +0330

    feat(Model): create find one by id and query builder in model class

--- core/database/Model.php
+++ core/database/Model.php
@@ -8,6 +8,17 @@ abstract class Model
 
     public static function all(): array
     {
-        return Database::query("SELECT * FROM " . static::table())->all();
+        return Database::query(self::queryBuilder("SELECT * FROM @tn"))->all();
+    }
+
+    public static function find(int $id): array
+    {
+        return Database::query(self::queryBuilder("SELECT * FROM @tn where id = $id"))->one();
+    }
+
+    private static function queryBuilder(string $query): string
+    {
+        $table = static::table();
+        return str_replace('@tn' , $table , $query);
     }
 }
