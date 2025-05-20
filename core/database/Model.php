a1d2e5f9eb3720d529a82707f81803cbe2ba97b1
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-05-20 13:43:16 +0330

    feat(Model): create crud trait for model

--- core/database/Model.php
+++ core/database/Model.php
@@ -2,60 +2,12 @@
 
 namespace Core\Database;
 
+use Core\Database\Traits\Crudable;
+
 abstract class Model
 {
+    use Crudable;
     abstract protected static function table(): string;
-
-    public static function all(): array
-    {
-        return Database::query(
-            self::queryBuilder("SELECT * FROM @tn")
-        )->all();
-    }
-
-    public static function find(int $id): array
-    {
-        return Database::query(
-            self::queryBuilder("SELECT * FROM @tn WHERE id = $id")
-        )->one();
-    }
-
-    public static function create(array $data): bool
-    {
-        $columns = implode(', ', array_keys($data));
-        $placeholders = ':' . implode(', :', array_keys($data));
-
-        return Database::query(
-                self::queryBuilder("INSERT INTO  @tn  ($columns) VALUES ($placeholders)"),
-                $data
-            )->count() > 0;
-    }
-
-    public static function delete(int $id): bool
-    {
-        return Database::query(
-            self::queryBuilder("DELETE FROM @tn WHERE id = $id"),
-        )->count() > 0;
-    }
-
-    public static function update(int $id, array $data): bool
-    {
-        $fields = implode(', ', array_map(fn($key) => "$key = :$key", array_keys($data)));
-        $data['id'] = $id;
-
-        return Database::query(
-                self::queryBuilder("UPDATE @tn SET $fields WHERE id = :id"),
-                $data
-            )->count() > 0;
-    }
-
-    public static function where(string $field, mixed $value): array
-    {
-        return Database::query(
-            self::queryBuilder("SELECT * FROM @tn WHERE $field = :value"),
-            ['value' => $value]
-        )->all();
-    }
 
     private static function queryBuilder(string $query): string
     {
---
+++ core/database/traits/Crudable.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Core\Database\Traits;
+
+use Core\Database\Database;
+
+trait Crudable
+{
+    public static function all(): array
+    {
+        return Database::query(
+            self::queryBuilder("SELECT * FROM @tn")
+        )->all();
+    }
+
+    public static function find(int $id): array
+    {
+        return Database::query(
+            self::queryBuilder("SELECT * FROM @tn WHERE id = $id")
+        )->one();
+    }
+
+    public static function create(array $data): bool
+    {
+        $columns = implode(', ', array_keys($data));
+        $placeholders = ':' . implode(', :', array_keys($data));
+
+        return Database::query(
+                self::queryBuilder("INSERT INTO  @tn  ($columns) VALUES ($placeholders)"),
+                $data
+            )->count() > 0;
+    }
+
+    public static function delete(int $id): bool
+    {
+        return Database::query(
+                self::queryBuilder("DELETE FROM @tn WHERE id = $id"),
+            )->count() > 0;
+    }
+
+    public static function update(int $id, array $data): bool
+    {
+        $fields = implode(', ', array_map(fn($key) => "$key = :$key", array_keys($data)));
+        $data['id'] = $id;
+
+        return Database::query(
+                self::queryBuilder("UPDATE @tn SET $fields WHERE id = :id"),
+                $data
+            )->count() > 0;
+    }
+
+    public static function where(string $field, mixed $value): array
+    {
+        return Database::query(
+            self::queryBuilder("SELECT * FROM @tn WHERE $field = :value"),
+            ['value' => $value]
+        )->all();
+    }
+
+}
