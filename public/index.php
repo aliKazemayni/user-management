2f93f284750761ecceb3693e0c9ca6300b46fe3b
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-08-17 20:58:11 +0330

    feat(Session): add start session in index

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use core\Router;
 use Dotenv\Dotenv;
 
+session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../routes/web.php';
 require_once __DIR__ . '/../core/helpers.php';
